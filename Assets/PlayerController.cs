b4489ce1395c1bf640f3816f818a0179e369d06c
Azrael <j.shepard.ramsay@gmail.com>
2015-06-23 23:24:32 -0400

    made player attachable on contact with host

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -29,21 +29,33 @@ public class PlayerController : MonoBehaviour {
 				transform.Translate(-Vector2.right * Time.deltaTime * moveSpeed);
 			}
 		}
-
+		//stand in, should make an "attached" boolean
+		if(canControl == false){
+			if(Input.GetKey(KeyCode.LeftControl)){
+				gameObject.transform.parent.DetachChildren();
+				canControl = true;
+			}
+		}
 		if(gameObject.GetComponent<PlayerStats>().hasHost == true){
 			//add host to list, follow host around
 		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
+
+	}
+	void OnTriggerStay2D(Collider2D other){
 		if(other.tag == "Host"){
 			canControl = false;
 			gameObject.GetComponent<PlayerStats>().hasHost = true;
-			transform.position = other.transform.position;
+			//transform.position = other.transform.position;
+			//other.transform
+			gameObject.transform.SetParent(other.transform);
 		}
 	}
 
 	void Hosted(){
+
 		//transform.position = 
 	}
 }
