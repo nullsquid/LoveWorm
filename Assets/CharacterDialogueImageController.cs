e506522301f7f2f512e61b264e5c9dcd029e6d91
Azrael <j.shepard.ramsay@gmail.com>
2015-06-29 23:27:44 -0400

    added sprites/image controller for dialogue rig

---
+++ Assets/CharacterDialogueImageController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterDialogueImageController : MonoBehaviour {
+
+
+	public GameObject activePortrait;
+	public GameObject passivePortrait;
+
+	//variables for positioning
+
+	void Awake(){
+		//activePortrait.transform.localPosition.y = Screen.height/2;
+	}
+
+	// Use this for initialization
+	void Start () {
+		//activePortrait.transform.localPosition = new Vector3(Screen.width/2, Screen.height/2, transform.localPosition.z);
+		//activePortrait.transform.position = Camera.main.ScreenToViewportPoint(new Vector3(Screen.width/2, Screen.height/2, transform.localPosition.z));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/SliderController.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+public class SliderController : MonoBehaviour {
+	
+	
+	public float characterDesireStrength;
+	public float characterDesireRate;
+	public float loveWormInfluenceTick;
+	
+	public float timeUntilFail;
+	
+	public bool isActive;
+	
+	public Character activeChar;
+	public Character passiveChar;
+	
+
+
+	public List<string> responseTones = new List<string>();
+	
+	public Slider slider;
+	public Slider timeCounter;
+	
+	void Awake(){
+		timeCounter.maxValue = timeUntilFail;
+		timeCounter.value = timeCounter.maxValue;
+	}
+	
+	void Start(){
+		StartCoroutine("Slide", characterDesireRate);
+	}
+	
+	void SlideUp(float amount){
+		slider.value += amount;
+	}
+	void SlideDown(float amount){
+		slider.value -= amount;
+	}
+	
+	void Fail(){
+		Debug.Log("Fail");
+		GetComponentInChildren<Canvas>().enabled = false;
+		
+	}
+	
+	IEnumerator Slide(float tickLength){
+		while(slider.value != slider.maxValue || slider.value != slider.minValue){
+			yield return new WaitForSeconds(tickLength);
+			SlideUp(characterDesireStrength);
+		}
+	}
+	void Update(){
+		timeUntilFail -= Time.deltaTime;
+		//timeCounter.value = timeUntilFail;
+		if(timeUntilFail <= 0){
+			//if(
+			//might want to make it a threshold thing
+			//If players are within range of the option they want then they get it when time runs out
+			Fail ();
+		}
+		
+		if(Input.GetKeyDown(KeyCode.LeftArrow)){
+			SlideDown(loveWormInfluenceTick);
+		}
+		else if(Input.GetKeyDown(KeyCode.RightArrow)){
+			SlideUp(loveWormInfluenceTick);
+		}
+		
+		if(slider.value >= slider.maxValue){
+			Debug.Log("option A");
+			StopCoroutine("Slide");
+			//slider.enabled = false;
+			//gameObject.enabled
+			//slider.
+			Fail ();
+			
+		}
+		else if (slider.value <= slider.minValue){
+			Debug.Log("option B");
+			StopCoroutine("Slide");
+			//slider.enabled = false;
+			Fail ();
+		}
+	}
+	void LateUpdate(){
+		timeCounter.value = timeUntilFail;
+	}
+}
